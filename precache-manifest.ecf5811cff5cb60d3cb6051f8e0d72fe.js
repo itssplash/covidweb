659827fcd2a47e30c9fb69774b2a045967ced8d6
Abhinav Jangra <51170452+itssplash@users.noreply.github.com>
2022-07-01 01:31:54 +0530

    Updates

---
+++ precache-manifest.ecf5811cff5cb60d3cb6051f8e0d72fe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "683e3a7fb680efbd1d8b0b8df89e1270",
+    "url": "/covidweb/index.html"
+  },
+  {
+    "revision": "3772ace463defbf36c68",
+    "url": "/covidweb/static/css/main.a4242303.chunk.css"
+  },
+  {
+    "revision": "5347977d62e38b6b1bf9",
+    "url": "/covidweb/static/js/2.44aa718d.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covidweb/static/js/2.44aa718d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3772ace463defbf36c68",
+    "url": "/covidweb/static/js/main.39ec3587.chunk.js"
+  },
+  {
+    "revision": "6b3fe40456cfee323a61",
+    "url": "/covidweb/static/js/runtime-main.fc8cbd9f.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covidweb/static/media/image.d7265326.png"
+  }
+]);
